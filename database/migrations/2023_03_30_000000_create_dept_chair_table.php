ea30850d5c49f26eeb42a15e8f727ed78bfa55b1
Christian Andrei <christianandreizpapa@gmail.com>
2023-04-20 01:51:21 +0800

    registration done

--- database/migrations/2023_03_30_000000_create_dept_chair_table.php
+++ database/migrations/2023_03_30_000000_create_dept_chair_table.php
@@ -14,14 +14,46 @@ return new class extends Migration
     public function up()
     {
         Schema::create('dept_chair', function (Blueprint $table) {
-            $table->integer('id')->primary();
-            $table->string('name');
+            $table->integer('id')->primary();                        
+            $table->string('lastname');
+            $table->string('firstname');
+            $table->string('middlename')->nullable();
+            $table->string('birth_date');
+            $table->string('birth_place');
+            $table->string('gender');
+            $table->string('civil_status');
+            $table->string('citizenship');
+            $table->string('house_number');
+            $table->string('street');
+            $table->string('subdivision');
+            $table->string('barangay');
+            $table->string('city');
+            $table->string('province');
+            $table->string('zipcode');
+            $table->string('gsis');
+            $table->string('pagibig');
+            $table->string('philhealth');
+            $table->string('sss');
+            $table->string('tin');
+            $table->string('agency_employee_no')->nullable();
+            $table->string('elementary_school')->nullable();
+            $table->string('elementary_remarks')->nullable();
+            $table->string('secondary_school')->nullable();
+            $table->string('secondary_remarks')->nullable();
+            $table->string('vocational_school')->nullable();
+            $table->string('vocational_remarks')->nullable();
+            $table->string('college_school')->nullable();
+            $table->string('college_remarks')->nullable();
+            $table->string('graduate_studies_school')->nullable();
+            $table->string('graduate_studies_remarks')->nullable();
+            $table->text('work_experiences')->nullable();
+            $table->string('email');
             $table->tinyInteger('user_type');
-            $table->string('status');
             $table->string('hash');
             $table->string('block_hash')->nullable();
+            $table->string('status');            
             $table->integer('created_by');
-            $table->integer('updated_by');
+            $table->integer('updated_by');            
             $table->timestamps();
         });
     }
--- database/migrations/2023_03_30_000000_create_student_table.php
+++ database/migrations/2023_03_30_000000_create_student_table.php
@@ -14,12 +14,39 @@ return new class extends Migration
     public function up()
     {
         Schema::create('student', function (Blueprint $table) {
-            $table->integer('id')->primary();
-            $table->string('name');
+            $table->integer('id')->primary();                        
+            $table->string('lastname');
+            $table->string('firstname');
+            $table->string('middlename')->nullable();
+            $table->string('extname')->nullable();
+            $table->string('birth_date');
+            $table->string('gender');
+            $table->string('address');
+            $table->string('civil_status');
+            $table->string('contact');
+            $table->string('is_cabuyeno');
+            $table->string('is_registered_voter');
+            $table->string('is_fully_vaccinated');
+            $table->string('father_name');
+            $table->string('father_occupation');
+            $table->string('father_contact');
+            $table->string('is_father_voter_of_cabuyao');
+            $table->string('mother_name');
+            $table->string('mother_occupation');
+            $table->string('mother_contact');
+            $table->string('is_mother_voter_of_cabuyao');
+            $table->string('is_living_with_parents');
+            $table->string('education_attained');
+            $table->string('last_school_attended');
+            $table->string('school_address');
+            $table->string('award_received');
+            $table->string('sh_school_strand');
+            $table->string('email');
+            $table->integer('course_id');
             $table->tinyInteger('user_type');
-            $table->string('status');
             $table->string('hash');
             $table->string('block_hash')->nullable();
+            $table->string('status');
             $table->integer('created_by');
             $table->integer('updated_by');
             $table->timestamps();
